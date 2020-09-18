67c907b756a390f6771c00c302e48bfdddd91f74
Seidmani64 <A01028650@itesm.mx>
2020-09-17 22:32:38 -0500

    Implemented distancia and centros functions

--- k_means.py
+++ k_means.py
@@ -1,21 +1,31 @@
+import numpy as np
+
 def distancia(lista1, lista2):
     sum = 0
 
     for i in range(len(lista1)):
         sum+=((lista2[i]-lista1[i])**2)
 
-    return (sum)**(1/2)
+    return np.sqrt(sum)
 
 def cercanos(puntos, centros):
-    pass
+    list = []
+    for x in range(len(centros)):
+        newList = []
+        list.append(newList)
 
 def centros(lista):
-    pass
+    centrosList = []
+    for i in range(len(lista)):
+        centrosList.append(np.average(lista[i]))
+    return centrosList
 
 def k_means(puntos):
     pass
 
 if __name__ == "__main__":
-    lista1 = [1,2,3]
-    lista2 = [4,5,6]
-    print(f"La distancia entre las listas es: {distancia(lista1,lista2)}")
+    #lista1 = [1,2,3]
+    #lista2 = [4,5,6]
+    #print(f"La distancia entre las listas es: {distancia(lista1,lista2)}")
+    #lista = [[1,2,3],[4,5,6]]
+    #print(f"Los centros son: {centros(lista)}")
